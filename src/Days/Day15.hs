d8d2632ea63eeb8dd53bc19b5ea9803974d0fb57
dixonary <alex@dixonary.co.uk>
2020-12-04 00:53:44 +0000

    Day 15 (incomplete)

--- src/Days/Day15.hs
+++ src/Days/Day15.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BinaryLiterals #-}
 module Days.Day15 (runDay) where
 
 {- ORMOLU_DISABLE -}
@@ -14,6 +15,14 @@ import qualified Util.Util as U
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
 import Data.Void
+
+import Control.Monad
+import Control.Monad.ST
+import Data.STRef
+import Data.Bits
+import Data.Function (on)
+import Text.Printf
+import Debug.Trace
 {- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
@@ -21,15 +30,44 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = do
+  string "Generator A starts with "
+  a <- decimal
+  skipSpace
+  string "Generator B starts with "
+  b <- decimal
+  return (a,b)
 
 ------------ TYPES ------------
-type Input = Void
+type Input = (Int,Int)
+
+imax = 2147483647
+
+-- This solution gives the right answers for
+-- a,b on the example data
+-- a on my own data
+-- NOT b on my own data :(
+
+observe propA propB iters (seedA,seedB) = let
+  mul n x = (x * n) `mod` imax
+  as = filter propA $ iterate (mul 16807) seedA
+  bs = filter propB $ iterate (mul 48271) seedB
+  sameLowest = (==) `on` (`mod` 2^16)
+  in foldl' (+) 0
+      $ map fromEnum
+      $ Prelude.take iters
+      $ zipWith sameLowest as bs
 
 ------------ PART A ------------
-partA :: Input -> Void
-partA = error "Not implemented yet!"
+partA :: Input -> Int
+partA = observe
+        (const True)
+        (const True)
+        40_000_000
 
 ------------ PART B ------------
-partB :: Input -> Void
-partB = error "Not implemented yet!"
+partB :: Input -> Int
+partB = observe
+        (\x -> (x `mod` 4) == 0)
+        (\x -> (x `mod` 8) == 0)
+        5_000_000
